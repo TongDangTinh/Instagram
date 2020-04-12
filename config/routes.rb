708e40d68de7b99bdf45a0b0ffaaa27c1fb8d631
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-04-12 21:18:29 +0700

    add public

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :accounts
+
+  get "/dashboard", to: "accounts#index"
   root "public#homepage"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
